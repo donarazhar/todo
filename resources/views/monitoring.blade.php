153e4b319de8192495f7d57cf410ca2e998695e2
donarazhar <donarazhar@github.com>
2026-06-12 12:44:08 +0700

    fix: set default tab to jadwal kegiatan

--- resources/views/monitoring.blade.php
+++ resources/views/monitoring.blade.php
@@ -259,7 +259,7 @@
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.3/dist/cdn.min.js"></script>
 </head>
 <body x-data="{
-    currentTab: 'progress',
+    currentTab: 'jadwal',
     modalOpen: false,
     modalType: '',
     modalData: {},
